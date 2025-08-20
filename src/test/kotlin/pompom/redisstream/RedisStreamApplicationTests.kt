2ad2e91ce162a8ef7ed837f759a1cf16ce355198
yslee1126 <yslee1126@gmail.com>
2025-08-20 20:03:57 +0900

    초기 커밋

---
+++ src/test/kotlin/pompom/redisstream/RedisStreamApplicationTests.kt
@@ -0,0 +1,13 @@
+package pompom.redisstream
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedisStreamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
